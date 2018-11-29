1239e7c52b2c2bcb40df62f17dbed68eafd7ead7
Paolo Meridiani <paolo.meridiani@cern.ch>
2018-11-29 12:53:26 +0100

    First running version

--- RecoLocalFastTime/Configuration/python/RecoLocalFastTime_cff.py
+++ RecoLocalFastTime/Configuration/python/RecoLocalFastTime_cff.py
@@ -7,8 +7,13 @@ fastTimingLocalReco = cms.Sequence(ftlUncalibratedRecHits*ftlRecHits)
 
 from RecoLocalFastTime.FTLRecProducers.mtdUncalibratedRecHits_cfi import mtdUncalibratedRecHits
 from RecoLocalFastTime.FTLRecProducers.mtdRecHits_cfi import mtdRecHits
-_phase2_timing_layer_new_fastTimingLocalReco = cms.Sequence(mtdUncalibratedRecHits*mtdRecHits)
+from RecoLocalFastTime.FTLClusterizer.mtdClusters_cfi import mtdClusters
+
+_phase2_timing_layer_new_fastTimingLocalReco = cms.Sequence(mtdUncalibratedRecHits*mtdRecHits+mtdClusters)
 from Configuration.Eras.Modifier_phase2_timing_layer_new_cff import phase2_timing_layer_new
 phase2_timing_layer_new.toReplaceWith(fastTimingLocalReco, _phase2_timing_layer_new_fastTimingLocalReco)
 phase2_timing_layer_new.toModify(mtdRecHits, barrelUncalibratedRecHits = cms.InputTag('mtdUncalibratedRecHits:FTLBarrel'), 
                                  endcapUncalibratedRecHits = cms.InputTag('mtdUncalibratedRecHits:FTLEndcap') )
+
+
+print phase2_timing_layer_new
